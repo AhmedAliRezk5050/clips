62e55404f580f03db9d40e37c1178dabb8538c44
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-16 04:55:16 +0200

    Create ModalService

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalService} from "../../services/modal.service";
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modalService: ModalService) { }
 
   ngOnInit(): void {
+    console.log(this.modalService.visible)
   }
 
 }
