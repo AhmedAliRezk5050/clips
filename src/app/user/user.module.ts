f6ebe30e055918e4bdd1ff0c8cfe76c5d9a7432d
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-16 03:42:24 +0200

    add user module + create components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AuthModalComponent],
+  exports: [AuthModalComponent],
 })
-export class UserModule { }
+export class UserModule {}
