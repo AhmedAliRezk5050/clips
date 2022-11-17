d4648f91f7500abeaf7ae1992263b8482bd79c84
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-17 22:16:07 +0200

    Update model + Create tabs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
   imports: [CommonModule],
-  exports: [CommonModule, FormsModule, ModalComponent],
+  exports: [CommonModule, FormsModule, ModalComponent, TabsContainerComponent, TabComponent],
 })
 export class SharedModule {}
