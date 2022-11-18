d6eaa1abe497e21728a6d4156589d7f9d10159f5
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-18 11:42:50 +0200

    dynamically change tab title

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, ContentChildren, OnInit, QueryList} from '@angular/core';
+import {TabComponent} from "../tab/tab.component";
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements OnInit, AfterContentInit {
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    this.tabs?.forEach(tab => {
+      console.log(tab)
+      debugger;
+    })
   }
 
 }
