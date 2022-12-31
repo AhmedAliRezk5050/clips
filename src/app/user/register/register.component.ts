107cad6066dbba1a6b5a9ee899ea6da576bb1116
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2022-12-31 11:20:50 +0300

    Forms Reactive - validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,12 +7,39 @@ import {FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-registerForm = new FormGroup({
-  name: new FormControl(''),
-  email: new FormControl(''),
-  age: new FormControl(''),
-  password: new FormControl(''),
-  confirmPassword: new FormControl(''),
-  phoneNumber: new FormControl(''),
-})
+  registerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl(''),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  })
+
+  get name() {
+    return this.registerForm.get('name')!
+  }
+
+  get email() {
+    return this.registerForm.get('email')!
+  }
+
+  get age() {
+    return this.registerForm.get('age')!
+  }
+
+  get password() {
+    return this.registerForm.get('password')!
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword')!
+  }
+
+  get phoneNumber() {
+    return this.registerForm.get('phoneNumber')!
+  }
 }
