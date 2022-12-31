9116df702606015f3479a9d90cac5fc165ecb3e0
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2022-12-31 09:52:50 +0300

    Forms Reactive

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,13 @@
-import { SharedModule } from './../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, ReactiveFormsModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
