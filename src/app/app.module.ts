5cff021e0236c100e3b70bc18df88f7c00056ad1
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2023-01-01 17:24:59 +0300

    firebase - auth Firestore Database - Store User Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {NavComponent} from './nav/nav.component';
 import {AngularFireModule} from "@angular/fire/compat";
 import {environment} from "../environments/environment";
 import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFireStorageModule} from "@angular/fire/compat/storage";
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
@@ -16,7 +17,8 @@ import {AngularFireAuthModule} from "@angular/fire/compat/auth";
     AppRoutingModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
 @Component({
   selector: 'app-register',
@@ -9,7 +10,8 @@ import {AngularFireAuth} from "@angular/fire/compat/auth";
 })
 export class RegisterComponent {
   submitBtnDisabled = false;
-  constructor(private auth: AngularFireAuth) {
+
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
   }
 
   name = new FormControl('', [
@@ -53,10 +55,16 @@ export class RegisterComponent {
     this.alertMsg = 'Please wait, your account is being created.';
     this.alertColor = 'blue';
     try {
-      const {email, password} = this.registerForm.value;
+      const {email, password, name, age, phoneNumber} = this.registerForm.value;
       const credentials = await this.auth.createUserWithEmailAndPassword(email!, password!)
       this.alertColor = 'green';
       this.alertMsg = 'Success!';
+      await this.db.collection('users').add({
+        name,
+        email,
+        age,
+        phoneNumber
+      });
       console.log(credentials);
     } catch (e: any) {
       console.error(e);
