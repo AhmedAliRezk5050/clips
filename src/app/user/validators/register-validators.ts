4ff5b119be3cb2bd2c2fb48d8827897404793fea
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2023-01-03 10:07:24 +0300

    Custom Validators Password match => connect the error to it's form input and show error message

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,16 +1,21 @@
 import {AbstractControl, ValidationErrors, ValidatorFn} from "@angular/forms";
 
 export class RegisterValidators {
-  static match(controlName: string, matchControlName: string): ValidatorFn  {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const password = control.get(controlName);
-      const confirmPassword = control.get(matchControlName);
+  static match(controlName: string, matchControlName: string): ValidatorFn {
+    return (abstractControl: AbstractControl): ValidationErrors | null => {
+      const control = abstractControl.get(controlName);
+      const matchingControl = abstractControl.get(matchControlName);
 
-      if (!password || !confirmPassword) {
+      if (!control || !matchingControl) {
+        // for developers not users
         return {controlNotFound: true}
       }
 
-      return password.value === confirmPassword.value ? null : {noMatch: true}
+      const error = control.value === matchingControl.value ? null : {noMatch: true}
+
+      matchingControl.setErrors(error);
+
+      return error;
     }
 
   }
