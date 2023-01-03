d448ad2ddc684d1ed76e5cf51e69b876fcc24a6a
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2023-01-03 11:25:17 +0300

    Custom Validators async Validators => check if email is taken => add delay to reduce network requests

--- src/app/user/validators/email-taken.ts
+++ src/app/user/validators/email-taken.ts
@@ -1,6 +1,9 @@
 import {AbstractControl, AsyncValidator, ValidationErrors} from "@angular/forms";
 import {Injectable} from "@angular/core";
 import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {catchError, delay, map, Observable, of, pipe, switchMap} from "rxjs";
+import {ajax} from "rxjs/internal/ajax/ajax";
+import {fromPromise} from "rxjs/internal/observable/innerFrom";
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +12,17 @@ export class EmailTaken implements AsyncValidator{
   constructor(private auth: AngularFireAuth) {
   }
 
-  validate = (control: AbstractControl): Promise<ValidationErrors | null> => {
-   return this.auth
-     .fetchSignInMethodsForEmail(control.value)
-     .then(response => response.length ?  {'emailTaken': true} :  null)
+  validate = (control: AbstractControl): Observable<ValidationErrors | null> => {
+    return of(control.value as string)
+      .pipe(
+        delay(500),
+        switchMap(email =>
+          fromPromise(this.auth.fetchSignInMethodsForEmail(email))
+          .pipe(
+            map(res => res.length ? {'emailTaken': true} : null),
+            catchError(err => of({'emailTaken': true}))
+          )
+        )
+      )
   }
 }
