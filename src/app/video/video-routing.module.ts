20b87162595fe6fe99c6b9c6b75951dc785e9c61
ahmedalirezk5050@gmail.com <ahmedalirezk5050@gmail.com>
2023-01-04 11:13:12 +0300

    Routing  Firebase Route Guards

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ManageComponent} from "./manage/manage.component";
 import {UploadComponent} from "./upload/upload.component";
+import {AngularFireAuthGuard, redirectUnauthorizedTo} from "@angular/fire/compat/auth-guard";
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/')
 
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AngularFireAuthGuard]
   },
   {
     path: 'upload',
     component: UploadComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AngularFireAuthGuard]
   },
   {
     path: 'manage-clips',
@@ -28,4 +35,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class VideoRoutingModule { }
+export class VideoRoutingModule {
+}
